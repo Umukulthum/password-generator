60feac2ab32bf738f4607391c32fe483261bcdc7
Umukulthum <kanyinsolaumukulthum@gmail.com>
2024-02-14 17:43:11 -0500

    Javascript code to generate a password based on the user's input

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,19 +88,89 @@ var upperCasedCharacters = [
     'Z'
 ];
 
+//Function to validate password length input
+function checkPasswordLength(length) {
+    if (!length || isNaN(length)) {
+        alert("Kindly input a valid number");
+        return false;
+    }
+
+    if (length < 8 || length > 128) {
+        alert("Password length has to be between 8 and 128");
+        return false;
+    }
+
+    return true;
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+    // Request and store desired password length from the user
+    let inputtedLength = prompt("Hello! \n\nKindly input the number of characters you want for your password");
 
+    //Validate inputted password length
+    if (!inputtedLength || !checkPasswordLength(inputtedLength)) {
+        return null;
+    }
+
+    //Convert input to integer
+    inputtedLength = parseInt(inputtedLength);
+
+    //Request and store desired password character combination from the user
+    let lowercaseCheck = confirm("Click OK to include lowercase characters");
+    let uppercaseCheck = confirm("Click OK to include uppercase characters");
+    let numericCheck = confirm("Click OK to include numerics");
+    let specialCharCheck = confirm("Click OK to include special characters");
+
+    //Store input in an array
+    const arrayCharacters = [
+        { characters: lowerCasedCharacters, isSelected: lowercaseCheck },
+        { characters: upperCasedCharacters, isSelected: uppercaseCheck },
+        { characters: numericCharacters, isSelected: numericCheck },
+        { characters: specialCharacters, isSelected: specialCharCheck }
+    ];
+
+    //Return the whole input collected as an object
+    return { groupedarray: arrayCharacters, passwordLength: inputtedLength };
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+    return Math.floor(Math.random() * arr.length);
 }
+
 
 // Function to generate password with user input
 function generatePassword() {
+    // Variable Declaration
+    let password = "";
+    let character = '';
 
+    //Get User inputs
+    let userInputArray = getPasswordOptions();
+
+    //Check if the inputs are valid
+    if (userInputArray !== null) {
+
+        let inputtedPasswordLength = userInputArray.passwordLength;
+        const inputtedSelections = userInputArray.groupedarray;
+
+        //Loop over the inputted length of password to generate password
+        while (password.length < inputtedPasswordLength) {
+            let arrayIndex = getRandom(inputtedSelections);
+
+            if (inputtedSelections[arrayIndex].isSelected) {
+                
+                let characterIndex = getRandom(inputtedSelections[arrayIndex].characters);
+                character = (inputtedSelections[arrayIndex].characters)[characterIndex];
+
+                password += character;
+            }
+        }
+    }
+
+    return password;
 }
 
 // Get references to the #generate element
